f85d321bfee59cce2f2442b4432c66e4c0f083c1
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-02-21 06:05:56 +0530

    Add @google/generative-ai dependency and update next version

---
+++ app/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+    return (
+        <>
+            <div class="border p-5 rounded-md border-gray text-white bg-[#212121] w-2/3">
+                <h1>Write a sassy poem</h1>
+                <p>It should have some good english words</p>
+            </div>
+        </>
+    )
+}
+
+export default card
